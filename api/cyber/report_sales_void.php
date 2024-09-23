5cc80aee5010dc72813e7ddb95f9daf242f2395d
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-09-23 15:22:20 +0700

    aaa

--- api/cyber/report_sales_void.php
+++ api/cyber/report_sales_void.php
@@ -6,6 +6,9 @@
 
 
 $query = "SELECT * FROM pos_dsalesdeleted WHERE isactived = '1' order by date(insertdate) desc";
+$query_qty = "SELECT * FROM pos_dsalesqtylog WHERE isactived = '1' order by date(insertdate) desc";
+
+
 
 $json = array();
 $no = 1;
@@ -45,6 +48,95 @@ foreach ($statement as $r) {
     $statement_cashier = $connec->query($query_cashier);
     foreach ($statement_cashier as $r_cashier) {
         $cashier_name = $r_cashier['username'];
+    }
+
+
+    $json[] = array(
+        "no" => $no,
+        "pos_dsalesdeleted_key" => $pos_dsalesdeleted_key,
+        "ad_mclient_key" => $ad_mclient_key,
+        "ad_morg_key" => $ad_morg_key,
+        "isactived" => $isactived,
+        "insertdate" => $insertdate,
+        "insertby" => $insertby,
+        "postby" => $postby,
+        "postdate" => $postdate,
+        "ad_muser_key" => $ad_muser_key,
+        "cashier_name" => $cashier_name,
+        "pos_dcashierbalance_key" => $pos_dcashierbalance_key,
+        "sku" => $sku,
+        "description" => $description,
+        "qty" => $qty,
+        "price" => $price,
+        "discount" => $discount,
+        "total" => $total,
+        "billno" => $billno,
+        "approvedby" => $approvedby,
+        "issync" => $issync,
+        "status_intransit" => $status_intransit
+    );
+
+    $no++;
+}
+
+$statement = $connec->query($query_qty);
+foreach ($statement as $r) {
+
+    // pos_dsalesqtylog_key
+// ad_mclient_key
+// ad_morg_key
+// isactived
+// insertdate
+// insertby
+// postby
+// postdate
+// ad_muser_key
+// pos_dcashierbalance_key
+// sku
+// oldqty
+// newqty
+// billno
+// approvedby
+
+    $pos_dsalesdeleted_key = $r['pos_dsalesqtylog_key'];
+    $ad_mclient_key = $r['ad_mclient_key'];
+    $ad_morg_key = $r['ad_morg_key'];
+    $isactived = $r['isactived'];
+    $insertdate = $r['insertdate'];
+    $insertby = $r['insertby'];
+    $postby = $r['postby'];
+    $postdate = $r['postdate'];
+    $ad_muser_key = $r['ad_muser_key'];
+    $pos_dcashierbalance_key = $r['pos_dcashierbalance_key'];
+    $sku = $r['sku'];
+    $qty = $r['newqty'] - $r['oldqty'];
+    $price = 0;
+    $discount = 0;
+    $billno = $r['billno'];
+    $approvedby = $r['approvedby'];
+    $issync = true;
+    $status_intransit = '1';
+    $total = 0;
+    $description = '';
+    //get description from ad_mproduct_key
+    $query_product = "SELECT * FROM pos_mproduct WHERE sku = '" . $sku . "' ";
+    $statement_product = $connec->query($query_product);
+    foreach ($statement_product as $r_product) {
+        $description = $r_product['name'];
+    }
+
+    $cashier_name = '';
+    //get cashier name from ad_muser_key
+    $query_cashier = "SELECT * FROM ad_muser WHERE ad_muser_key = '" . $ad_muser_key . "' ";
+    $statement_cashier = $connec->query($query_cashier);
+    foreach ($statement_cashier as $r_cashier) {
+        $cashier_name = $r_cashier['username'];
+    }
+
+    $query_cashier = "SELECT * FROM ad_muser WHERE ad_muser_key = '" . $approvedby . "' ";
+    $statement_cashier = $connec->query($query_cashier);
+    foreach ($statement_cashier as $r_cashier) {
+        $approvedby = $r_cashier['username'];
     }
 
 
--- api/cyber/restore_sales.php
+++ api/cyber/restore_sales.php
@@ -3,28 +3,28 @@ $ll = "select * from ad_morg where isactived = 'Y'";
 $query = $connec->query($ll);
 
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    $idstore = $row['ad_morg_key'];
+	$idstore = $row['ad_morg_key'];
 }
 function get_sales($url)
 {
-    $curl = curl_init();
-    curl_setopt_array(
-        $curl,
-        array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        )
-    );
+	$curl = curl_init();
+	curl_setopt_array(
+		$curl,
+		array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+		)
+	);
 
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
+	$response = curl_exec($curl);
+	curl_close($curl);
+	return $response;
 }
 $url = $base_url . '/store/restore/restore_sales.php?idstore=' . $idstore;
 
@@ -37,11 +37,21 @@ $s = array();
 
 
 try {
-    // $connec->beginTransaction();
 
-    foreach ($j_hasil['sales'] as $key => $r) {
+	//delete pos_dsales_log where date now
+	$delete_pos_dsales_log = "delete from pos_dsales_log where date(insertdate) = date(now())";
+	$connec->query($delete_pos_dsales_log);
 
-        $qq_header = "insert into pos_dsales (pos_dsales_key, ad_mclient_key, ad_morg_key, isactived, insertdate, insertby, postby, postdate, pos_medc_key, pos_dcashierbalance_key, pos_mbank_key, ad_muser_key, billno, billamount, 
+	//delete pos_dsalesline_log where date now
+	$delete_pos_dsalesline_log = "delete from pos_dsalesline_log where date(insertdate) = date(now())";
+	$connec->query($delete_pos_dsalesline_log);
+	
+
+	// $connec->beginTransaction();
+
+	foreach ($j_hasil['sales'] as $key => $r) {
+
+		$qq_header = "insert into pos_dsales (pos_dsales_key, ad_mclient_key, ad_morg_key, isactived, insertdate, insertby, postby, postdate, pos_medc_key, pos_dcashierbalance_key, pos_mbank_key, ad_muser_key, billno, billamount, 
 		paymentmethodname, membercard, cardno, approvecode, edcno, bankname, serialno, billstatus, paycashgiven, paygiven, printcount, issync, donasiamount, dpp, ppn, billcode, ispromomurah, point, pointgive, membername, status_intransit) 
 		values (
 		'" . $r['pos_dsales_key'] . "', 
@@ -81,53 +91,53 @@ try {
 		'" . $r['status_intransit'] . "'
 		)";
 
-        // print_r($r['line']);
+		// print_r($r['line']);
 
-        $result = $connec->query($qq_header);
-        if ($result) {
-            foreach ($r['line'] as $r_line) {
+		$result = $connec->query($qq_header);
+		if ($result) {
+			foreach ($r['line'] as $r_line) {
 
-                $qq_line = "INSERT INTO pos_dsalesline
-		(pos_dsalesline_key, ad_mclient_key, ad_morg_key, isactived, insertdate, insertby, postby, postdate, pos_dsales_key, billno, seqno, sku, qty, price, 
-		discount, amount, issync, discountname, status_sales, status_intransit) 
-		values (
-		'" . $r_line['pos_dsalesline_key'] . "', 
-		'" . $r_line['ad_mclient_key'] . "', 
-		'" . $r_line['ad_morg_key'] . "', 
-		'" . $r_line['isactived'] . "', 
-		'" . $r_line['insertdate'] . "', 
-		'" . $r_line['insertby'] . "', 
-		'" . $r_line['postby'] . "', 
-		'" . $r_line['postdate'] . "', 
-		'" . $r_line['pos_dsales_key'] . "', 
-		'" . $r_line['billno'] . "', 
-		'" . $r_line['seqno'] . "', 
-		'" . $r_line['sku'] . "', 
-		'" . $r_line['qty'] . "', 
-		'" . $r_line['price'] . "', 
-		'" . $r_line['discount'] . "', 
-		'" . $r_line['amount'] . "', 
-		true, 
-		'" . $r_line['discountname'] . "', 
-		'" . $r_line['status_sales'] . "', 
-		'" . $r_line['status_intransit'] . "'
-		)";
+				$qq_line = "INSERT INTO pos_dsalesline
+					(pos_dsalesline_key, ad_mclient_key, ad_morg_key, isactived, insertdate, insertby, postby, postdate, pos_dsales_key, billno, seqno, sku, qty, price, 
+					discount, amount, issync, discountname, status_sales, status_intransit) 
+					values (
+					'" . $r_line['pos_dsalesline_key'] . "', 
+					'" . $r_line['ad_mclient_key'] . "', 
+					'" . $r_line['ad_morg_key'] . "', 
+					'" . $r_line['isactived'] . "', 
+					'" . $r_line['insertdate'] . "', 
+					'" . $r_line['insertby'] . "', 
+					'" . $r_line['postby'] . "', 
+					'" . $r_line['postdate'] . "', 
+					'" . $r_line['pos_dsales_key'] . "', 
+					'" . $r_line['billno'] . "', 
+					'" . $r_line['seqno'] . "', 
+					'" . $r_line['sku'] . "', 
+					'" . $r_line['qty'] . "', 
+					'" . $r_line['price'] . "', 
+					'" . $r_line['discount'] . "', 
+					'" . $r_line['amount'] . "', 
+					true, 
+					'" . $r_line['discountname'] . "', 
+					'" . $r_line['status_sales'] . "', 
+					'" . $r_line['status_intransit'] . "'
+					)";
 
-               $connec->query($qq_line);
+				$connec->query($qq_line);
 
-            }
+			}
 
-            // echo $qq_line;
-
-            
-
-        }
-    }
+			// echo $qq_line;
 
 
-    foreach ($j_hasil['cashier_balance'] as $key => $r) {
 
-        $qq_cashier_balance = "insert into pos_dcashierbalance (pos_dcashierbalance_key, ad_mclient_key, ad_morg_key, isactived, insertdate, insertby, postby, postdate, pos_mcashier_key, ad_muser_key, pos_mshift_key, startdate, enddate, 
+		}
+	}
+
+
+	foreach ($j_hasil['cashier_balance'] as $key => $r) {
+
+		$qq_cashier_balance = "insert into pos_dcashierbalance (pos_dcashierbalance_key, ad_mclient_key, ad_morg_key, isactived, insertdate, insertby, postby, postdate, pos_mcashier_key, ad_muser_key, pos_mshift_key, startdate, enddate, 
 		balanceamount, salesamount, status, salescashamount, salesdebitamount, salescreditamount, actualamount, issync, refundamount, discountamount, cancelcount, cancelamount, donasiamount, pointamount, pointdebitamout, pointcreditamount)
 		values (
 		'" . $r['pos_dcashierbalance_key'] . "', 
@@ -161,22 +171,22 @@ try {
 		" . (($r['pointcreditamount'] == '') ? "NULL" : ("'" . $r['pointcreditamount'] . "'")) . "
 		)";
 
-        $result = $connec->query($qq_cashier_balance);
+		$result = $connec->query($qq_cashier_balance);
 
-    }
+	}
 
-    // $connec->commit();
-
-
-    $json = array(
-        "status" => "SUCCESS",
-        "message" => "Data Inserted",
-    );
+	// $connec->commit();
 
 
-    echo json_encode($json);
+	$json = array(
+		"status" => "SUCCESS",
+		"message" => "Data Inserted",
+	);
+
+
+	echo json_encode($json);
 } catch (Exception $e) {
-    $connec->rollBack();
+	$connec->rollBack();
 }
 
 
