02afdfa403953dc0f10de28fa182572588fa1b89
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-09-23 10:58:07 +0700

    aaa

--- api/cyber/report_sales_today.php
+++ api/cyber/report_sales_today.php
@@ -3,18 +3,17 @@
 
 
 $namestore = '';
+$ad_org_id = '';
 $ll = "select * from ad_morg where isactived = 'Y'";
 $query = $connec->query($ll);
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
     $namestore = $row['name'];
+    $ad_org_id = $row['ad_morg_key'];
 }
-
-
-
 
 //group by date(insertdate)
 $json = array();
-$groupdate = "SELECT date(insertdate) as date FROM pos_dsales WHERE isactived = '1' group by date(insertdate)";
+$groupdate = "SELECT date(insertdate) as date FROM pos_dsales WHERE isactived = '1' and ad_morg_key = '".$ad_org_id."' group by date(insertdate)";
 $statement_groupdate = $connec->query($groupdate);
 foreach ($statement_groupdate as $r_groupdate) {
     $date = $r_groupdate['date'];
@@ -22,13 +21,13 @@ foreach ($statement_groupdate as $r_groupdate) {
     $std = 0;
     $apc = 0;
 
-    $query_omset = "SELECT sum(qty*price) omset FROM pos_dsalesline WHERE isactived = '1' and date(insertdate) = date(now())";
+    $query_omset = "SELECT sum(qty*price) omset FROM pos_dsalesline WHERE isactived = '1' and ad_morg_key = '" . $ad_org_id . "' and date(insertdate) = '".$date."'";
     $statement_omset = $connec->query($query_omset);
     foreach ($statement_omset as $r_omset) {
         $omset = $r_omset['omset'];
     }
 
-    $query_std = "SELECT count(billno) std FROM pos_dsales WHERE isactived = '1' and date(insertdate) = date(now())";
+    $query_std = "SELECT count(billno) std FROM pos_dsales WHERE isactived = '1' and ad_morg_key = '" . $ad_org_id . "' and date(insertdate) = '".$date."'";
     $statement_std = $connec->query($query_std);
     foreach ($statement_std as $r_std) {
         $std = $r_std['std'];
